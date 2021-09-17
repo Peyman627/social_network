6e7b2dcb9aa1deb71303f07749abf8c8e22cce78
Peyman Hassani <hassani.peyman627@gmail.com>
2021-09-17 10:17:28 +0430

    Setup posts app and add to settings

--- social_network/settings.py
+++ social_network/settings.py
@@ -30,6 +30,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'users.apps.UsersConfig',
+    'posts.apps.PostsConfig',
     'rest_framework',
     'rest_framework_simplejwt',
     'rest_framework_simplejwt.token_blacklist',
