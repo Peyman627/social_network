3399a63b3ff1eb13d0ae9ce6337c255523afe0e1
Peyman Hassani <hassani.peyman627@gmail.com>
2021-09-17 17:50:14 +0430

    Update admin panel for articles, messages and posts apps

--- articles/admin.py
+++ articles/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import (Article, ArticleComment, ArticleImage, ArticleTag,
+                     ArticleVote)
+
+admin.site.register(Article)
+admin.site.register(ArticleComment)
+admin.site.register(ArticleImage)
+admin.site.register(ArticleTag)
+admin.site.register(ArticleVote)
