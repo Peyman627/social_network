915219a49c0afb7e3dc3db43ff08490ff3788994
Peyman Hassani <hassani.peyman627@gmail.com>
2021-09-18 11:55:07 +0430

    Add partial hyperlinking to posts app apis
    user field on serializers is not linked

--- posts/serializers.py
+++ posts/serializers.py
@@ -1,15 +1,40 @@
 from rest_framework import serializers
+from rest_framework.reverse import reverse
 
 from .models import Post, PostLike
 
 
+class PostLikeHyperlinkedRelatedField(serializers.HyperlinkedRelatedField):
+    view_name = 'posts:post_like_detail'
+
+    def get_url(self, obj, view_name, request, format):
+        url_kwargs = {'post_id': obj.post.id, 'like_id': obj.id}
+        return reverse(view_name,
+                       kwargs=url_kwargs,
+                       request=request,
+                       format=format)
+
+    def get_object(self, view_name, view_args, view_kwargs):
+        lookup_kwargs = {
+            'post': view_kwargs['post_id'],
+            'id': view_kwargs['like_id']
+        }
+        return self.get_queryset().get(**lookup_kwargs)
+
+
 class PostSerializer(serializers.ModelSerializer):
+    url = serializers.HyperlinkedIdentityField(view_name='posts:post_detail',
+                                               lookup_url_kwarg='post_id',
+                                               read_only=True)
+    parent = serializers.HyperlinkedRelatedField(view_name='posts:post_detail',
+                                                 lookup_url_kwarg='post_id',
+                                                 read_only=True)
     likes_count = serializers.SerializerMethodField(read_only=True)
 
     class Meta:
         model = Post
         fields = [
-            'id', 'parent', 'user', 'likes', 'likes_count', 'content', 'image',
+            'url', 'parent', 'user', 'likes_count', 'content', 'image',
             'created_time'
         ]
 
@@ -18,6 +43,11 @@ class PostSerializer(serializers.ModelSerializer):
 
 
 class PostLikeSerializer(serializers.ModelSerializer):
+    url = PostLikeHyperlinkedRelatedField(source='*', read_only=True)
+    post = serializers.HyperlinkedRelatedField(view_name='posts:post_detail',
+                                               lookup_url_kwarg='post_id',
+                                               read_only=True)
+
     class Meta:
         model = PostLike
-        fields = ['id', 'user', 'post', 'created_time']
+        fields = ['url', 'user', 'post', 'created_time']
