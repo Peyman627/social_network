134cdab17178ffc120dcfa468cf9c15f40a4e21e
Peyman Hassani <hassani.peyman627@gmail.com>
2021-09-20 14:01:11 +0430

    Update articles admin panel

--- articles/admin.py
+++ articles/admin.py
@@ -3,8 +3,34 @@ from django.contrib import admin
 from .models import (Article, ArticleComment, ArticleImage, ArticleTag,
                      ArticleVote)
 
-admin.site.register(Article)
+
+class ArticleCommentInline(admin.TabularInline):
+    model = ArticleComment
+    extra = 1
+
+
+class ArticleImageInline(admin.TabularInline):
+    model = ArticleImage
+    extra = 1
+
+
+class ArticleVoteInline(admin.TabularInline):
+    model = ArticleVote
+    extra = 1
+
+
+@admin.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ('username', 'title', 'created_time', 'updated_time')
+    list_display_links = ('username', )
+    date_hierarchy = 'created_time'
+    inlines = (ArticleCommentInline, ArticleImageInline, ArticleVoteInline)
+
+    @admin.display
+    def username(self, obj):
+        return obj.user.username
+
+
 admin.site.register(ArticleComment)
 admin.site.register(ArticleImage)
-admin.site.register(ArticleTag)
 admin.site.register(ArticleVote)
